fb5794fe8095ef88e2e3c3ce3adb9c61fabfd97f
pedrofnseca <pedropereira.pp264@gmail.com>
2022-12-15 22:16:15 -0300

    :fire: delete: exemplo email

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { MailService } from './mail/mail.service';
-import { SMTPMailService } from './mail/smtp-mail.service';
-import { PostMarkMailService } from './mail/postMark-mail.service'
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [{
-    provide: MailService,
-    useClass: PostMarkMailService
-  }],
+  providers: [
+  ],
 })
 export class AppModule {}
